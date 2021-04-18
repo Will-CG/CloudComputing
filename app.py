9fc9068f6da3e6a815cf06aa21f6a1d34ec18df2
Will-CG <80711569+Will-CG@users.noreply.github.com>
2021-04-18 02:08:17 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,101 +1,55 @@
-# This file is based on: https://raw.githubusercontent.com/streamlit/demo-uber-nyc-pickups/master/app.py
-
-"""An example of showing geographic data."""
-
-import streamlit as st
-import pandas as pd
 import numpy as np
-import altair as alt
-import pydeck as pdk
+import pandas as pd
+import streamlit as st
+from pandas_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
-DATE_TIME = "date/time"
-DATA_URL = (
-    "http://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz"
-)
-TOTAL_SAMPLES = 100000
+# Web App Title
+st.markdown('''
+# **Store data Analysis**
 
-st.title("Uber Pickups in New York City")
-st.markdown(
-    """
-This is a demo of a Streamlit app that shows the Uber pickups
-geographical distribution in New York City. Use the slider
-to pick a specific hour and look at how the charts change.
+This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
 
-[See source code](https://github.com/streamlit/demo-uber-nyc-pickups/blob/master/app.py)
-"""
-)
+**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat)
 
+---
+''')
 
-@st.cache(persist=True)
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    data.rename(lambda x: str(x).lower(), axis="columns", inplace=True)
-    data[DATE_TIME] = pd.to_datetime(data[DATE_TIME])
-    return data
+# Upload CSV data
+with st.sidebar.header('1. Upload your CSV data'):
+    uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+    st.sidebar.markdown("""
+[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
+""")
 
-
-def run():
-    data = load_data(TOTAL_SAMPLES)
-
-    hour = st.slider("Hour to look at", 0, 23)
-
-    data = data[data[DATE_TIME].dt.hour == hour]
-
-    st.subheader("Geo data between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-    midpoint = (np.average(data["lat"]), np.average(data["lon"]))
-
-    st.write(
-        pdk.Deck(
-            map_style="mapbox://styles/mapbox/light-v9",
-            initial_view_state={
-                "latitude": midpoint[0],
-                "longitude": midpoint[1],
-                "zoom": 11,
-                "pitch": 50,
-            },
-            layers=[
-                pdk.Layer(
-                    "HexagonLayer",
-                    data=data,
-                    get_position=["lon", "lat"],
-                    radius=100,
-                    elevation_scale=4,
-                    elevation_range=[0, 1000],
-                    pickable=True,
-                    extruded=True,
-                ),
-            ],
-        )
-    )
-
-    st.subheader(
-        "Breakdown by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
-    )
-    filtered = data[
-        (data[DATE_TIME].dt.hour >= hour) & (data[DATE_TIME].dt.hour < (hour + 1))
-    ]
-    hist = np.histogram(filtered[DATE_TIME].dt.minute, bins=60, range=(0, 60))[0]
-    chart_data = pd.DataFrame({"minute": range(60), "pickups": hist})
-
-    st.altair_chart(
-        alt.Chart(chart_data)
-        .mark_area(
-            interpolate="step-after",
-        )
-        .encode(
-            x=alt.X("minute:Q", scale=alt.Scale(nice=False)),
-            y=alt.Y("pickups:Q"),
-            tooltip=["minute", "pickups"],
-        ),
-        use_container_width=True,
-    )
-
-    if st.checkbox("Show raw data", False):
-        st.subheader(
-            "Raw data by minute between %i:00 and %i:00" % (hour, (hour + 1) % 24)
-        )
-        st.write(data)
-
-
-if __name__ == "__main__":
-    run()
+# Pandas Profiling Report
+if uploaded_file is not None:
+    @st.cache
+    def load_csv():
+        csv = pd.read_csv(uploaded_file)
+        return csv
+    df = load_csv()
+    pr = ProfileReport(df, explorative=True)
+    st.header('**Input DataFrame**')
+    st.write(df)
+    st.write('---')
+    st.header('**Pandas Profiling Report**')
+    st_profile_report(pr)
+else:
+    st.info('Awaiting for CSV file to be uploaded.')
+    if st.button('Press to use Example Dataset'):
+        # Example data
+        @st.cache
+        def load_data():
+            a = pd.DataFrame(
+                np.random.rand(100, 5),
+                columns=['a', 'b', 'c', 'd', 'e']
+            )
+            return a
+        df = load_data()
+        pr = ProfileReport(df, explorative=True)
+        st.header('**Input DataFrame**')
+        st.write(df)
+        st.write('---')
+        st.header('**Pandas Profiling Report**')
+        st_profile_report(pr)
